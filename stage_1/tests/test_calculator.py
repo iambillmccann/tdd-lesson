e7062afa67a2da740066ba50789d028127710925
Bill McCann <bill.mccann@gmail.com>
2024-03-19 16:23:53 -0400

    Initial code

---
+++ stage_1/tests/test_calculator.py
@@ -0,0 +1,5 @@
+import pytest
+from src.calculator import add
+
+def test_add():
+    assert add(2, 3) == 5
---
+++ stage_3/tests/test_calculator.py
@@ -0,0 +1,11 @@
+import pytest
+from src.calculator import add
+
+def test_add_positive_numbers():
+    assert add(2, 3) == 5
+
+def test_add_negative_numbers():
+    assert add(-2, -3) == -5
+
+def test_add_positive_and_negative_number():
+    assert add(-2, 3) == 1
